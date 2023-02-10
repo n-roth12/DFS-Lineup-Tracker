921d0b944578952b1bad63975a0cf8b424e4463f
Nolan Roth <nolanrroth12@gmail.com>
2023-02-10 10:40:37 -0800

    Improving borders on create lineup page.

--- src/components/LineupPlayerNewV2/LineupPlayerNewV2.js
+++ src/components/LineupPlayerNewV2/LineupPlayerNewV2.js
@@ -6,72 +6,70 @@ import { CgArrowsExchange } from 'react-icons/cg'
 function LineupPlayerNew({ player, position, beingEdited, onDelete, onAdd, onOpenDialog, toggleEditingPos, editingPos, setPlayerDialogContent }) {
 
   return (
-    <div>
-      <div className={`player lineupPlayerNewV2 ${beingEdited ? 'selected': ''}`}>
-        <div className='playerImage'>
-            <p>{position.toUpperCase()}</p>
-        </div>
-        <div className='name-and-info'>
-            <div className='playerName'>
-                <p onClick={() => setPlayerDialogContent(player)}>
-                    {player["firstName"]} {player["lastName"]}
-                </p>
-            </div>
-            <div className='playerInfo'>
-                <div className='infoBlock'>
-                    <p>
-                    <span className='value'>
-                        {parseFloat(player['fppg']).toFixed(2)}
-                    </span>
-                    <span className='label'>
-                        FPPG
-                    </span>
-                    </p>
-                </div>
-                <div className='infoBlock'>
-                    <p>
-                    <span className='value'>
-                        {player["game"]["awayTeam"]} @ {player["game"]["homeTeam"]}
-                    </span>
-                    <span className='label'>
-                        GAME
-                    </span>
-                    </p>
-                </div>
-                <div className='infoBlock'>
-                    <p>
-                    <span className='value'>
-                        {player['oprk']}
-                    </span>
-                    <span className='label'>
-                        OPRK
-                    </span>
-                    </p>
-                </div>
-                <div className='infoBlock'>
-                    <p>
-                    <span className='value'>${player["salary"]}</span>
-                    <span className='label'>SAL</span>
-                    </p>
-                </div>
-            </div>
-        </div>
-        
-          {!beingEdited ?
-            <div className='icon' onClick={() => toggleEditingPos(position)}>
-              <div>
-                <CgArrowsExchange className='add-icon' />
+    <div className={`player lineupPlayerNewV2 ${beingEdited ? 'selected': ''}`}>
+      <div className='playerImage'>
+          <p>{position.toUpperCase()}</p>
+      </div>
+      <div className='name-and-info'>
+          <div className='playerName'>
+              <p onClick={() => setPlayerDialogContent(player)}>
+                  {player["firstName"]} {player["lastName"]}
+              </p>
+          </div>
+          <div className='playerInfo'>
+              <div className='infoBlock'>
+                  <p>
+                  <span className='value'>
+                      {parseFloat(player['fppg']).toFixed(2)}
+                  </span>
+                  <span className='label'>
+                      FPPG
+                  </span>
+                  </p>
               </div>
-            </div>
-          :
-          <div className='icon' onClick={() => onDelete(position)}>
+              <div className='infoBlock'>
+                  <p>
+                  <span className='value'>
+                      {player["game"]["awayTeam"]} @ {player["game"]["homeTeam"]}
+                  </span>
+                  <span className='label'>
+                      GAME
+                  </span>
+                  </p>
+              </div>
+              <div className='infoBlock'>
+                  <p>
+                  <span className='value'>
+                      {player['oprk']}
+                  </span>
+                  <span className='label'>
+                      OPRK
+                  </span>
+                  </p>
+              </div>
+              <div className='infoBlock'>
+                  <p>
+                  <span className='value'>${player["salary"]}</span>
+                  <span className='label'>SAL</span>
+                  </p>
+              </div>
+          </div>
+      </div>
+      
+        {!beingEdited ?
+          <div className='icon' onClick={() => toggleEditingPos(position)}>
             <div>
-              <FaTimes className='delete-icon' />
+              <CgArrowsExchange className='add-icon' />
             </div>
           </div>
-          }
-        </div> 
-    </div>
+        :
+        <div className='icon' onClick={() => onDelete(position)}>
+          <div>
+            <FaTimes className='delete-icon' />
+          </div>
+        </div>
+        }
+      </div> 
   )
 }
 
