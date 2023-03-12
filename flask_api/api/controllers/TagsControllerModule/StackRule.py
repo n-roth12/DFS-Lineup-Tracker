95bb3b1e03e822b382dbf6f21264897f0ae51018
Nolan Roth <nolanrroth12@gmail.com>
2023-03-11 22:05:57 -0800

    Dynamically check for stacks in StackRule.

--- flask_api/api/controllers/TagsControllerModule/StackRule.py
+++ flask_api/api/controllers/TagsControllerModule/StackRule.py
@@ -1,29 +1,52 @@
 from LineupOptimizerControllerModule.Lineup import Lineup
 
+stack_types = [(3, 1), (3, 2), (4, 1), (4, 2), (4, 3), (5, 1), (3, 0), (4, 0), (2, 1), (5, 0)]
+
 class StackRule:
 
-    def check_rule(self, lineup):
-        team_positional_map = self.get_team_positional_map(lineup)
-        
+    def check_rule(lineup: Lineup) -> list:
+        stack_map = StackRule.get_team_positional_map(lineup=lineup)
+        return StackRule.get_stacks(stack_map)
 
-    def get_team_positional_map(self, lineup):
-        result = {}
+    def implement_rule(lineup: Lineup):
+        return 
 
-        for player in lineup.values():
-            if player.get("game").get("gameId") not in result.keys():
-                result[player.get("game").get("gameId")] = { 
-                    player.get("game").get("homeTeam"): [], 
-                    player.get("game").get("awayTeam") : [] 
-                }
-            result[player.get("game").get("gameId")][player.get("team")].append(player)
+    # def contains_stack(self, stack_map: dict, num_players1: int, num_players2: int):
+    #     result = []
+    #     for gameId in stack_map.keys():
+    #         if (len(stack_map[gameId]["homeTeam"]["players"]) == num_players1 and len(stack_map[gameId]["awayTeam"]["players"]) == num_players2)  \
+    #             or (len(stack_map[gameId]["awayTeam"]["players"]) == num_players1 and len(stack_map[gameId]["homeTeam"]["players"]) == num_players2):
+    #                 return True
+    #     return False
+
+    def get_stacks(stack_map: dict):
+        result = []
+        for gameId in stack_map.keys():
+            players1 = max(len(stack_map[gameId]["homeTeam"]["players"]), len(stack_map[gameId]["awayTeam"]["players"]))
+            players2 = min(len(stack_map[gameId]["homeTeam"]["players"]), len(stack_map[gameId]["awayTeam"]["players"]))
+
+            if (players1, players2) in stack_types:
+                result.append((players1, players2))
 
         return result
 
 
+    def get_team_positional_map(lineup: Lineup):
+        stack_map = {}
 
-# maybe store game map like this:
-# stack_map = { 
-#   174834: { "homeTeam": { "team": "TEN", "players": [] }, "awayTeam": { "team": "SF", players: [{player1}] }
-#   899028: { "homeTeam": { "team": "MIA": "players": [{player1}], "awayTeam" :{ "team": "BUF", "players": [{player1}, {player2}, {player3}] }
-# }
+        for player in [x for x in lineup.lineup.values() if x]:
+            if player.get("game").get("gameId") not in stack_map.keys():
+                stack_map[player.get("game").get("gameId")] = \
+                { 
+                    "homeTeam": {"team": player.get("game").get("homeTeam"), 
+                    "players": [player] if player.get("team") == player.get("game").get("homeTeam") else []}, 
+                    "awayTeam": {"team": player.get("game").get("awayTeam"), "players": [],
+                    "players": [player] if player.get("team") == player.get("game").get("awayTeam") else []}
+                }
+            else:
+                if player.get("team") == player.get("game").get("homeTeam"):
+                    stack_map[player.get("game").get("gameId")]["homeTeam"]["players"].append(player)
+                else:
+                    stack_map[player.get("game").get("gameId")]["awayTeam"]["players"].append(player)
 
+        return stack_map
