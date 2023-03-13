f4a728acbb9020160f101372c3c8859d0bcfb5af
Nolan Roth <nolanrroth12@gmail.com>
2023-03-13 15:23:16 -0700

    More test cases for builder.

--- flask_api/api/controllers/LineupOptimizerControllerModule/LineupBuilder.py
+++ flask_api/api/controllers/LineupOptimizerControllerModule/LineupBuilder.py
@@ -11,11 +11,22 @@ class LineupBuilder:
     def get(self, position_title: str):
         return next((lineup_slot for lineup_slot in self.lineup_slots if lineup_slot.title == position_title), None)
     
-    def with_composition_rule(self, composition_dict: dict):
-        return
+    def with_composition_rule(self, position: str, num_players: int):
+        count = 0
+        for slot in self.lineup_slots:
+            if position in slot.eligible_positions:
+                count += 1
+                if len(slot.eligible_positions) > 1:
+                    slot.eligible_positions = [position]
+                if count >= num_players:
+                    return self
+
+        return None
 
     def with_stack_rule(self, stack_dict: dict):
-        return
+        return self
 
-    def with_punt_rule(self, position: str, salary_max = None):
-        return
+    def with_punt_rule(self, position: str, max_salary = None):
+        p = self.get(position)
+        p.set_max_salary(max_salary)
+        return self
--- flask_api/api/controllers/TagsControllerModule/LineupBuilderTests.py
+++ flask_api/api/controllers/TagsControllerModule/LineupBuilderTests.py
@@ -10,10 +10,29 @@ class LineupBuilderTests(unittest.TestCase):
     def test_lineup_builder_get(self):
         lineup_builder = LineupBuilder(positions=["QB", "RB1", "RB2", "WR1", "WR2", "WR3", "TE", "FLEX", "DST"], site="draftkings")
         self.assertIsNone(lineup_builder.get("TE2"))
-        self.assertIsNotNone(lineup_builder.get("TE"))
         self.assertTrue(lineup_builder.get("WR3").title == "WR3")
-        self.assertFalse(lineup_builder.get("FLEX").title == "RB1")
 
-        
+    def test_with_punt_rule(self):
+        lineup_builder = LineupBuilder(positions=["QB", "RB1", "RB2", "WR1", "WR2", "WR3", "TE", "FLEX", "DST"], 
+            site="draftkings").with_punt_rule("TE", 4000).with_punt_rule("WR2", 3500)
+        self.assertEqual(4000, lineup_builder.get("TE").max_salary)
+        self.assertEqual(3500, lineup_builder.get("WR2").max_salary)
 
-    
+    def test_with_valid_composition_rule(self):
+        lineup_builder = LineupBuilder(positions=["QB", "RB1", "RB2", "WR1", "WR2", "WR3", "TE", "FLEX", "DST"], 
+            site="draftkings").with_composition_rule(position="RB", num_players=3)    
+        self.assertEqual(["RB"], lineup_builder.get("FLEX").eligible_positions)
+        lineup_builder = LineupBuilder(positions=["QB", "RB1", "RB2", "WR1", "WR2", "WR3", "TE", "FLEX", "DST"], 
+            site="draftkings").with_composition_rule(position="WR", num_players=4)    
+        lineup_builder = LineupBuilder(positions=["QB", "RB1", "RB2", "WR1", "WR2", "WR3", "TE", "FLEX", "DST"], 
+            site="draftkings").with_composition_rule(position="TE", num_players=2)   
+
+    def test_with_invalid_composition_rule(self):
+        self.assertIsNone(LineupBuilder(positions=["QB", "RB1", "RB2", "WR1", "WR2", "WR3", "TE", "FLEX", "DST"], 
+            site="draftkings").with_composition_rule(position="RB", num_players=4))
+        self.assertIsNone(LineupBuilder(positions=["QB", "RB1", "RB2", "WR1", "WR2", "WR3", "TE", "FLEX", "DST"], 
+            site="draftkings").with_composition_rule(position="WR", num_players=5))
+        self.assertIsNone(LineupBuilder(positions=["QB", "RB1", "RB2", "WR1", "WR2", "WR3", "TE", "FLEX", "DST"], 
+            site="draftkings").with_composition_rule(position="TE", num_players=3))
+        self.assertIsNone(LineupBuilder(positions=["QB", "RB1", "RB2", "WR1", "WR2", "WR3", "TE", "FLEX", "DST"], 
+            site="draftkings").with_composition_rule(position="QB", num_players=2))
