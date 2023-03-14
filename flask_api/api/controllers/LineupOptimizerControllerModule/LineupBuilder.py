a2e6d7d46dbb26de58360ce48e01b60404ba0519
Nolan Roth <nolanrroth12@gmail.com>
2023-03-13 20:42:59 -0700

    LineupBuilder now creates valid Lineups.

--- flask_api/api/controllers/LineupOptimizerControllerModule/LineupBuilder.py
+++ flask_api/api/controllers/LineupOptimizerControllerModule/LineupBuilder.py
@@ -1,18 +1,34 @@
 from LineupOptimizerControllerModule.LineupBuilderSlot import LineupBuilderSlot
+from LineupOptimizerControllerModule.PlayerPicker import PlayerPicker
+from LineupOptimizerControllerModule.allowed_positions import injured_status_list
+from LineupOptimizerControllerModule.Lineup import Lineup
+from random import randint
 
 STACK_ORDER = ["QB", "WR", "TE", "RB", "DST"]
+NUM_PLAYERS_TO_CONSIDER = 10
 
 class LineupBuilder:
 
-    def __init__(self, positions: list, site: str):
+    def __init__(self, positions: list, site: str, draftables: list):
         self.lineup_slots = []
         self.site = site
         for position in positions:
             self.lineup_slots.append(LineupBuilderSlot(title=position, site=site))
+        self.weighted_cost_map = self.create_weighted_cost_map(draftables)
 
     def get(self, position_title: str) -> LineupBuilderSlot:
         return next((lineup_slot for lineup_slot in self.lineup_slots if lineup_slot.title == position_title), None)
-    
+
+    def build(self) -> Lineup:
+        lineup = {}
+        lineup_ids = []
+        for lineup_slot in self.lineup_slots:
+            player = self.pick_player(lineup_slot.eligible_positions[0], 
+                team_abbr=lineup_slot.eligible_team, max_salary=lineup_slot.max_salary, taken_ids=lineup_ids)
+            lineup_ids.append(player["playerSiteId"])
+            lineup[lineup_slot.title] = player
+        return Lineup(lineup=lineup, site=self.site)
+
     def with_composition_rule(self, position: str, num_players: int):
         count = 0
         for slot in self.lineup_slots:
@@ -53,5 +69,28 @@ class LineupBuilder:
                     count1 += 1
         return None
 
-    def stack_order_helper(self, eligible_positions: list):
+    def stack_order_helper(self, eligible_positions: list) -> int:
         return min([STACK_ORDER.index(pos) for pos in eligible_positions])
+
+    def create_weighted_cost_map(self, draftables: list) -> dict:
+        weighted_cost_map = {}
+        for draftable in draftables:
+            fantasy_points_per_game = 0.0 if draftable["fppg"] == "-" else float(draftable["fppg"]) 
+            value = fantasy_points_per_game / int(draftable["salary"])
+            draftable["value"] = value
+            if draftable.get("position") not in weighted_cost_map.keys():
+                weighted_cost_map[draftable["position"]] = [draftable]
+            else:
+                weighted_cost_map[draftable["position"]].append(draftable)
+        for position in weighted_cost_map.keys():
+            weighted_cost_map[position].sort(key=lambda player : player["value"], reverse=True)
+        return weighted_cost_map
+
+    # picks the top players that matches the filters, None if no player matches filter
+    def pick_player(self, position: str, taken_ids = [], team_abbr = None, max_salary = None) -> dict:
+        return next((player for player in self.weighted_cost_map[position] if (
+            (team_abbr == None or player["team"] == team_abbr) 
+            and (max_salary == None or player["salary"] <=  max_salary) 
+            and (player["status"] not in injured_status_list)
+            and (player["playerSiteId"] not in taken_ids)
+            )), None)
--- flask_api/api/controllers/TagsControllerModule/LineupBuilderTests.py
+++ flask_api/api/controllers/TagsControllerModule/LineupBuilderTests.py
@@ -1,6 +1,7 @@
 import unittest
 from LineupOptimizerControllerModule.LineupBuilder import LineupBuilder
 from LineupOptimizerControllerModule.LineupBuilderSlot import LineupBuilderSlot
+from LineupOptimizerControllerModule.test_draftables import test_draftables
 
 class LineupBuilderTests(unittest.TestCase):
 
@@ -8,39 +9,33 @@ class LineupBuilderTests(unittest.TestCase):
         return self.assertTrue(True)
 
     def test_lineup_builder_get(self):
-        lineup_builder = LineupBuilder(positions=["QB", "RB1", "RB2", "WR1", "WR2", "WR3", "TE", "FLEX", "DST"], site="draftkings")
+        lineup_builder = LineupBuilder(positions=["QB", "RB1", "RB2", "WR1", "WR2", "WR3", "TE", "FLEX", "DST"], 
+            site="draftkings", draftables=test_draftables)
         self.assertIsNone(lineup_builder.get("TE2"))
         self.assertTrue(lineup_builder.get("WR3").title == "WR3")
 
     def test_with_punt_rule(self):
         lineup_builder = LineupBuilder(positions=["QB", "RB1", "RB2", "WR1", "WR2", "WR3", "TE", "FLEX", "DST"], 
-            site="draftkings").with_punt_rule("TE", 4000).with_punt_rule("WR2", 3500)
+            site="draftkings", draftables=test_draftables).with_punt_rule("TE", 4000).with_punt_rule("WR2", 3500)
         self.assertEqual(4000, lineup_builder.get("TE").max_salary)
         self.assertEqual(3500, lineup_builder.get("WR2").max_salary)
 
     def test_with_valid_composition_rule(self):
-        lineup_builder = LineupBuilder(positions=["QB", "RB1", "RB2", "WR1", "WR2", "WR3", "TE", "FLEX", "DST"], 
-            site="draftkings").with_composition_rule(position="RB", num_players=3)    
+        lineup_builder = self.default_draftkings_builder().with_composition_rule(position="RB", num_players=3)    
         self.assertEqual(["RB"], lineup_builder.get("FLEX").eligible_positions)
-        lineup_builder = LineupBuilder(positions=["QB", "RB1", "RB2", "WR1", "WR2", "WR3", "TE", "FLEX", "DST"], 
-            site="draftkings").with_composition_rule(position="WR", num_players=4)    
-        lineup_builder = LineupBuilder(positions=["QB", "RB1", "RB2", "WR1", "WR2", "WR3", "TE", "FLEX", "DST"], 
-            site="draftkings").with_composition_rule(position="TE", num_players=2)   
+        lineup_builder = self.default_draftkings_builder().with_composition_rule(position="WR", num_players=4)    
+        self.assertEqual(["WR"], lineup_builder.get("FLEX").eligible_positions)
+        lineup_builder = self.default_draftkings_builder().with_composition_rule(position="TE", num_players=2)   
+        self.assertEqual(["TE"], lineup_builder.get("FLEX").eligible_positions)
 
     def test_with_invalid_composition_rule(self):
-        self.assertIsNone(LineupBuilder(positions=["QB", "RB1", "RB2", "WR1", "WR2", "WR3", "TE", "FLEX", "DST"], 
-            site="draftkings").with_composition_rule(position="RB", num_players=4))
-        self.assertIsNone(LineupBuilder(positions=["QB", "RB1", "RB2", "WR1", "WR2", "WR3", "TE", "FLEX", "DST"], 
-            site="draftkings").with_composition_rule(position="WR", num_players=5))
-        self.assertIsNone(LineupBuilder(positions=["QB", "RB1", "RB2", "WR1", "WR2", "WR3", "TE", "FLEX", "DST"], 
-            site="draftkings").with_composition_rule(position="TE", num_players=3))
-        self.assertIsNone(LineupBuilder(positions=["QB", "RB1", "RB2", "WR1", "WR2", "WR3", "TE", "FLEX", "DST"], 
-            site="draftkings").with_composition_rule(position="QB", num_players=2))
+        self.assertIsNone(self.default_draftkings_builder().with_composition_rule(position="RB", num_players=4))
+        self.assertIsNone(self.default_draftkings_builder().with_composition_rule(position="WR", num_players=5))
+        self.assertIsNone(self.default_draftkings_builder().with_composition_rule(position="TE", num_players=3))
+        self.assertIsNone(self.default_draftkings_builder().with_composition_rule(position="QB", num_players=2))
 
     def test_with_valid_stack_rule_3_1(self):
-        lineup_builder = LineupBuilder(positions=["QB", "RB1", "RB2", "WR1", "WR2", "WR3", "TE", "FLEX", "DST"], 
-            site="draftkings").with_stack_rule("KC", 3, "JAX", 1)
-
+        lineup_builder = self.default_draftkings_builder().with_stack_rule("KC", 3, "JAX", 1)
         self.assertTrue(lineup_builder.get("QB").eligible_team == "KC")
         self.assertTrue(lineup_builder.get("WR1").eligible_team == "KC")
         self.assertTrue(lineup_builder.get("WR2").eligible_team == "JAX")
@@ -48,9 +43,7 @@ class LineupBuilderTests(unittest.TestCase):
         self.assertTrue(lineup_builder.get("FLEX").eligible_team == None)
 
     def test_with_valid_stack_rule_1_3(self):
-        lineup_builder = LineupBuilder(positions=["QB", "RB1", "RB2", "WR1", "WR2", "WR3", "TE", "FLEX", "DST"], 
-            site="draftkings").with_stack_rule("KC", 1, "JAX", 3)
-
+        lineup_builder = self.default_draftkings_builder().with_stack_rule("KC", 1, "JAX", 3)
         self.assertTrue(lineup_builder.get("QB").eligible_team == "KC")
         self.assertTrue(lineup_builder.get("WR1").eligible_team == "JAX")
         self.assertTrue(lineup_builder.get("WR2").eligible_team == "JAX")
@@ -58,9 +51,7 @@ class LineupBuilderTests(unittest.TestCase):
         self.assertTrue(lineup_builder.get("FLEX").eligible_team == None)
 
     def test_with_valid_stack_rule_2_2(self):
-        lineup_builder = LineupBuilder(positions=["QB", "RB1", "RB2", "WR1", "WR2", "WR3", "TE", "FLEX", "DST"], 
-            site="draftkings").with_stack_rule("KC", 2, "JAX", 2)
-
+        lineup_builder = self.default_draftkings_builder().with_stack_rule("KC", 2, "JAX", 2)
         self.assertTrue(lineup_builder.get("QB").eligible_team == "KC")
         self.assertTrue(lineup_builder.get("WR1").eligible_team == "KC")
         self.assertTrue(lineup_builder.get("WR2").eligible_team == "JAX")
@@ -68,18 +59,14 @@ class LineupBuilderTests(unittest.TestCase):
         self.assertTrue(lineup_builder.get("FLEX").eligible_team == None)
 
     def test_with_valid_stack_rule_3_0(self):
-        lineup_builder = LineupBuilder(positions=["QB", "RB1", "RB2", "WR1", "WR2", "WR3", "TE", "FLEX", "DST"], 
-            site="draftkings").with_stack_rule("KC", 3, "JAX", 0)
-
+        lineup_builder = self.default_draftkings_builder().with_stack_rule("KC", 3, "JAX", 0)
         self.assertTrue(lineup_builder.get("QB").eligible_team == "KC")
         self.assertTrue(lineup_builder.get("WR1").eligible_team == "KC")
         self.assertTrue(lineup_builder.get("WR2").eligible_team == "KC")
         self.assertTrue(lineup_builder.get("WR3").eligible_team == None)
 
     def test_with_valid_stack_rule_4_3(self):
-        lineup_builder = LineupBuilder(positions=["QB", "RB1", "RB2", "WR1", "WR2", "WR3", "TE", "FLEX", "DST"], 
-            site="draftkings").with_stack_rule("KC", 4, "JAX", 3)
-
+        lineup_builder = self.default_draftkings_builder().with_stack_rule("KC", 4, "JAX", 3)
         self.assertTrue(lineup_builder.get("QB").eligible_team == "KC")
         self.assertTrue(lineup_builder.get("WR1").eligible_team == "KC")
         self.assertTrue(lineup_builder.get("WR2").eligible_team == "JAX")
@@ -90,14 +77,43 @@ class LineupBuilderTests(unittest.TestCase):
         self.assertTrue(lineup_builder.get("RB2").eligible_team == None)
 
     def test_with_valid_stack_rule_0_0(self):
-        lineup_builder = LineupBuilder(positions=["QB", "RB1", "RB2", "WR1", "WR2", "WR3", "TE", "FLEX", "DST"], 
-            site="draftkings").with_stack_rule("KC", 0, "JAX", 0)
-
+        lineup_builder = self.default_draftkings_builder().with_stack_rule("KC", 0, "JAX", 0)
         self.assertTrue(lineup_builder.get("QB").eligible_team == None)
 
     def test_with_invalid_stack_rule_5_5(self):
-        lineup_builder = LineupBuilder(positions=["QB", "RB1", "RB2", "WR1", "WR2", "WR3", "TE", "FLEX", "DST"], 
-            site="draftkings").with_stack_rule("KC", 5, "JAX", 5)
-
+        lineup_builder = self.default_draftkings_builder().with_stack_rule("KC", 5, "JAX", 5)
         self.assertIsNone(lineup_builder)
-    
+
+    def test_pick_valid_player_1(self):
+        lineup_builder = self.default_draftkings_builder()
+        player = lineup_builder.pick_player(position="WR", team_abbr="JAX", max_salary=4000)
+        self.assertEqual("WR", player.get("position"))
+        self.assertEqual("JAX", player.get("team"))
+        self.assertGreaterEqual(4000, player.get("salary"))
+
+    def test_pick_valid_player_2(self):
+        lineup_builder = self.default_draftkings_builder()
+        player = lineup_builder.pick_player(position="TE", team_abbr="KC")
+        self.assertEqual("TE", player.get("position"))
+        self.assertEqual("KC", player.get("team"))
+
+    def test_pick_valid_player_2(self):
+        lineup_builder = self.default_draftkings_builder()
+        player = lineup_builder.pick_player(position="RB")
+        self.assertEqual("RB", player.get("position"))
+
+    def test_pick_invalid_player(self):
+        self.assertIsNone(self.default_draftkings_builder().pick_player(position="WR", team_abbr="KI", max_salary=4000))
+
+    def test_build(self):
+        lineup = self.default_draftkings_builder().build()
+        self.assertIsNotNone(lineup)
+        for slot in lineup.lineup.keys():
+            print(slot, lineup.get(slot))
+
+        
+### HELPER METHODS ###
+
+    def default_draftkings_builder(self):
+        return LineupBuilder(positions=["QB", "RB1", "RB2", "WR1", "WR2", "WR3", "TE", "FLEX", "DST"], 
+            site="draftkings", draftables=test_draftables)
