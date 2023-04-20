e1bb34e483c8d2bd12c19d276b4f5756dd54ced2
Nolan Roth <nolanrroth12@gmail.com>
2023-04-20 14:20:20 -0700

    New burger menu.

--- src/components/Navbar/MobileNav/MobileNav.js
+++ src/components/Navbar/MobileNav/MobileNav.js
@@ -1,17 +1,48 @@
 import './MobileNav.scss'
-import { fallDown as Menu } from "react-burger-menu"
-import { Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { FaAngleRight } from 'react-icons/fa'
 
 const MobileNav = () => {
+
+  const [burgerClass, setBurgerClass] = useState("burger-bar unclicked")
+  const [menuClass, setMenuClass] = useState("menu hidden")
+  const [isMenuClicked, setIsMenuClicked] = useState(false)
+
+  const updateMenu = () => {
+    if (!isMenuClicked) {
+      setBurgerClass("burger-bar clicked")
+      setMenuClass("menu visible")
+    } else {
+      setBurgerClass("burger-bar unclicked")
+      setMenuClass("menu hidden")
+    }
+    setIsMenuClicked(!isMenuClicked)
+  }
+
+  const logout = () => {
+    localStorage.clear();
+    sessionStorage.clear();
+  }
+
   return (
     <div className='mobile-nav'>
-    <Menu className='bt-burger-button' noOverlay>
-      <Link className='player-link' to={`/lineups`}>Lineups</Link>
-      <Link className='player-link' to={`/upcoming`}>Upcoming</Link>
-      <Link className='player-link' to={`/history`}>Players</Link>
-      <Link className='player-link' to={`/login`}>Logout</Link>
-    </Menu>
-  </div>
+      <nav>
+        <h1>Mainslater</h1>
+        <div className='burger-menu' onClick={updateMenu}>
+          <div className={burgerClass}></div>
+          <div className={burgerClass}></div>
+          <div className={burgerClass}></div>
+        </div>
+      </nav>
+
+      <div className={menuClass}>
+        <Link className='nav-link' to={`/lineups`}>Lineups <FaAngleRight /></Link>
+        <Link className='nav-link' to={`/upcoming`}>Upcoming <FaAngleRight /></Link>
+        <Link className='nav-link' to={`/history`}>Players <FaAngleRight /></Link>
+        <Link className='nav-link' to={'/login'} onClick={logout}>Logout <FaAngleRight /></Link>
+      </div>
+    </div>
   )
 }
 
